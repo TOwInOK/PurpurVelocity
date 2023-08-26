0854228cdfe6a873a7761ff8a1a664c48779901f
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2023-08-27 02:01:57 +0700

    disable sha256 checker
    cause PaperMC...

--- getvelocity.sh
+++ getvelocity.sh
@@ -12,26 +12,37 @@ echo "--------------------------------------------------------------------------
 
 # Construct the download URL
 DOWNLOAD_URL="https://api.papermc.io/v2/projects/velocity/versions/${LATEST_VERSION}/builds/${LATEST_BUILD}/downloads/${LATEST_VERSION}-${LATEST_BUILD}.jar"
-
-# Download the Purpur server JAR
-
-while true; do
-  # download
-  curl -O "$DOWNLOAD_URL"
-  echo `ls .`
-  #mathing sha
-  downloaded_sha256=$(sha256sum "${LATEST_VERSION}-${LATEST_BUILD}.jar" | awk '{print $1}')
-  # Compair SHA256
-  if [ "$downloaded_sha256" = "${SHA256}" ]; then
-    echo "Velocity server downloaded successfully."
-    mv ${LATEST_VERSION}-${LATEST_BUILD}.jar velocity.jar
-    break
-  else
-    echo "Velocity server downloaded with error"
-    echo "${downloaded_sha256} << download"
-    echo "${SHA256} << SAVED"
-    SHA256=$(curl -s "https://api.papermc.io/v2/projects/velocity/versions/${LATEST_VERSION}/builds/")
-    rm velocity.jar
+curl -s -o velocity.jar "$DOWNLOAD_URL"
+downloaded_sha256=$(sha256sum "velocity.jar" | awk '{print $1}')
+if [ "$downloaded_sha256" = "${SHA256}" ]; then
+  echo "Velocity server downloaded successfully."
+else
+     echo "Velocity server downloaded with error"
+     echo "${downloaded_sha256} << download"
+     echo "${SHA256} << SAVED"
+     echo "be caraful it's Paper Team..."
   fi
 done
+#code down bellow is work but not for files from PaperMC...
+# Download the Purpur server JAR
+# while true; do
+#   # download
+#   curl -O "$DOWNLOAD_URL"
+#   echo `ls .`
+#   #mathing sha
+#  downloaded_sha256=$(sha256sum "velocity.jar" | awk '{print $1}')
+#   # Compair SHA256
+#   if [ "$downloaded_sha256" = "${SHA256}" ]; then
+#     echo "Velocity server downloaded successfully."
+#     mv ${LATEST_VERSION}-${LATEST_BUILD}.jar velocity.jar
+#     break
+#   else
+#     echo "Velocity server downloaded with error"
+#     echo "${downloaded_sha256} << download"
+#     echo "${SHA256} << SAVED"
+#     SHA256=$(curl -s "https://api.papermc.io/v2/projects/velocity/versions/${LATEST_VERSION}/builds/")
+#     rm velocity.jar
+#   fi
+# done
+
 
