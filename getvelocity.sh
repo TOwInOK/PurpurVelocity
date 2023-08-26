a5defcbbc3016afc425f7a45f29a33fd5f016bea
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2023-08-27 03:59:37 +0700

    recover checker

--- getvelocity.sh
+++ getvelocity.sh
@@ -11,37 +11,28 @@ echo "Velocity build: $LATEST_BUILD Latest version: $LATEST_VERSION"
 echo "--------------------------------------------------------------------------------------------------------"
 
 # Construct the download URL
-DOWNLOAD_URL="https://api.papermc.io/v2/projects/velocity/versions/${LATEST_VERSION}/builds/${LATEST_BUILD}/downloads/${LATEST_VERSION}-${LATEST_BUILD}.jar"
+DOWNLOAD_URL="https://api.papermc.io/v2/projects/velocity/versions/${LATEST_VERSION}/builds/${LATEST_BUILD}/downloads/velocity-${LATEST_VERSION}-${LATEST_BUILD}.jar"
 curl -s -o velocity.jar "$DOWNLOAD_URL"
 downloaded_sha256=$(sha256sum "velocity.jar" | awk '{print $1}')
-if [ "$downloaded_sha256" = "${SHA256}" ]; then
-  echo "Velocity server downloaded successfully."
-else
-     echo "Velocity server downloaded with error"
-     echo "${downloaded_sha256} << download"
-     echo "${SHA256} << SAVED"
-     echo "be caraful it's Paper Team..."
-fi
-#code down bellow is work but not for files from PaperMC...
-# Download the Purpur server JAR
-# while true; do
-#   # download
-#   curl -O "$DOWNLOAD_URL"
-#   echo `ls .`
-#   #mathing sha
-#  downloaded_sha256=$(sha256sum "velocity.jar" | awk '{print $1}')
-#   # Compair SHA256
-#   if [ "$downloaded_sha256" = "${SHA256}" ]; then
-#     echo "Velocity server downloaded successfully."
-#     mv ${LATEST_VERSION}-${LATEST_BUILD}.jar velocity.jar
-#     break
-#   else
-#     echo "Velocity server downloaded with error"
-#     echo "${downloaded_sha256} << download"
-#     echo "${SHA256} << SAVED"
-#     SHA256=$(curl -s "https://api.papermc.io/v2/projects/velocity/versions/${LATEST_VERSION}/builds/")
-#     rm velocity.jar
-#   fi
-# done
+
+Download the Purpur server JAR
+while true; do
+  # download
+  curl -O "$DOWNLOAD_URL"
+  echo `ls .`
+  #mathing sha
+ downloaded_sha256=$(sha256sum "velocity.jar" | awk '{print $1}')
+  # Compair SHA256
+  if [ "$downloaded_sha256" = "${SHA256}" ]; then
+    echo "Velocity server downloaded successfully."
+    mv ${LATEST_VERSION}-${LATEST_BUILD}.jar velocity.jar
+    break
+  else
+    echo "Velocity server downloaded with error"
+    echo "${downloaded_sha256} << download"
+    echo "${SHA256} << SAVED"
+    rm velocity.jar
+  fi
+done
 
 
